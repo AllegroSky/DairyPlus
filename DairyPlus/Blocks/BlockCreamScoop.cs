15f945c1f2e3ca1f9b25d8156ab178fc7e5b8449
AllegroSky <skypiea@gmail.com>
2026-04-22 07:11:09 -0500

    update to cream scoop, goes by 10L increments now

--- DairyPlus/Blocks/BlockCreamScoop.cs
+++ DairyPlus/Blocks/BlockCreamScoop.cs
@@ -30,11 +30,11 @@ namespace DairyPlus.Items
             float curLitres = liqslot.Itemstack.StackSize / itemsPerLitre;
             if (liqslot.Itemstack.Item.Code.Path == "item/liquid/separatingmilk") return;
 
-                //has enough milk
-            if (curLitres < 50f)
+                //has enough milk + is mutiple of 10
+            if (curLitres < 10f || curLitres % 10f != 0f)
             {
-                (api as ICoreClientAPI)?.TriggerIngameError(this, "notenough",
-                    Lang.Get("Need 50L of Settled Milk to skim Cream from"));
+                (api as ICoreClientAPI)?.TriggerIngameError(this, "wrong amount",
+                    Lang.Get("Need 10, 20, 30, 40, or 50L of Settled Milk to skim Cream from"));
                 return;
             }
 
@@ -60,43 +60,48 @@ namespace DairyPlus.Items
 
         public override bool OnHeldInteractStep(float secondsUsed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel)
         {
-            //Wait 2ish seconds of "processing time"
-            return secondsUsed < 1.7f;
+            //Wait for "processing time"
+            return secondsUsed < 1.5f;
         }
 
 
         public override void OnHeldInteractStop(float secondsUsed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel)
         {
-            if (secondsUsed < 1.7f || blockSel == null) return;
+            if (secondsUsed < 1.5f || blockSel == null) return;
 
             // define variables again 
             var beba = api.World.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityBarrel;
             var liqslot = beba?.Inventory[1];
             var milkProps = GetContainableProps(liqslot?.Itemstack);
             float itemsPerLitre = milkProps?.ItemsPerLitre ?? 1f;
-            float curLitres = (liqslot?.Itemstack != null)
-             ? liqslot.Itemstack.StackSize / itemsPerLitre : 0f;
+            int batchLitres = (int)(liqslot.Itemstack.StackSize / itemsPerLitre);
 
+            //math
+            int creamLitres = (int)(batchLitres * 0.2);
+            int skimLitres = (int)(batchLitres * 0.8);
             // makes server do this
 
             if (api.World.Side == EnumAppSide.Server)
             {
-                // Remove milk
-                liqslot.TakeOut((int)(50f * itemsPerLitre));
+                TransitionableProperties perishProps = GetPerishProps(liqslot.Itemstack);
 
                 // Fill scoop 
                 Item creamItem = api.World.GetItem(new AssetLocation("dairyplus", "cream"));
                 if (creamItem != null)
                 {
                     ItemStack source = new ItemStack(creamItem, 9999);
-                    TryPutLiquid(slot.Itemstack, source, 10f);
+                    CarryOverFreshness(api, liqslot, source, perishProps);
+                    TryPutLiquid(slot.Itemstack, source, creamLitres);
                 }
 
-                // add skim milk 
+                // remove milk add skim milk 
                 Item skimItem = api.World.GetItem(new AssetLocation("dairyplus", "skimmilk"));
                 if (skimItem != null)
                 {
-                    liqslot.Itemstack = new ItemStack(skimItem, (int)(40f * itemsPerLitre));
+                    ItemStack skimStack = new ItemStack(skimItem, skimLitres * (int)itemsPerLitre);
+                    CarryOverFreshness(api, liqslot, skimStack, perishProps);
+                    liqslot.TakeOut((int)(batchLitres * itemsPerLitre));
+                    liqslot.Itemstack = skimStack;
                 }
 
                 // tell client to update
@@ -104,12 +109,27 @@ namespace DairyPlus.Items
                 beba.MarkDirty(true);
                 slot.MarkDirty();
             }
-                // stop progress bar
+            // stop progress bar
             if (byEntity.World.Side == EnumAppSide.Client)
             {
                 byEntity.Controls.HandUse = EnumHandInteract.None;
             }
         }
+        //freshness checker helper
+        private TransitionableProperties GetPerishProps(ItemStack stack)
+        {
+            var props = stack?.Collectible?.GetTransitionableProperties(api.World, stack, null);
+            if (props == null) return null;
 
+            foreach (var p in props)
+            {
+                if (p.Type == EnumTransitionType.Perish)
+                {
+                    return p;
+                }
+            }
+
+            return null;
+        }
     }
 }
